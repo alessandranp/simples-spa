d4864cd3d70a96c0e0b47fef8ffd03af0874db49
La017 <luizm4artins@hotmail.com>
2021-09-18 10:14:43 -0300

    errosintax

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
 const BTN_CADASTRAR = document.grtElementById('btn-cadastrar');
 const BTN_LISTAR = document.grtElementById('btn-listar');
-const SECTION_CADASTRAR = document.grtElementById('btn-cadastrar');
-const SECTION_LISTAR = document.grtElementById('btn-listar');
+const SECTION_CADASTRAR = document.grtElementById('section-cadastrar');
+const SECTION_LISTAR = document.grtElementById('section-listar');
 
 BTN_LISTAR.addEventListener('click', function(){
   SECTION_CADASTRAR.style.display = 'none';
