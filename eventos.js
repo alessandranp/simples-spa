8620c5f512d39ed81f2ef13a7ca9e8bc04a542e9
La017 <luizm4artins@hotmail.com>
2021-09-18 10:10:20 -0300

    evento funcao

--- eventos.js
+++ eventos.js
@@ -2,3 +2,13 @@ const BTN_CADASTRAR = document.grtElementById('btn-cadastrar');
 const BTN_LISTAR = document.grtElementById('btn-listar');
 const SECTION_CADASTRAR = document.grtElementById('btn-cadastrar');
 const SECTION_LISTAR = document.grtElementById('btn-listar');
+
+BTN_LISTAR.addEventListener('click', function(){
+  SECTION_CADASTRAR.style.display = 'none';
+  SECTION_LISTAR.style.display = 'block';
+});
+
+BTN_CADASTRAR.addEventListener('click', function(){
+    SECTION_LISTAR.style.display = 'none';
+    SECTION_CADASTRAR.style.display = 'block';
+});
