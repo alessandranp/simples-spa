c0617e52fbb47a7ceb991fa431dc5d9e3882e6ae
La017 <luizm4artins@hotmail.com>
2021-09-18 10:32:50 -0300

    errosintax

--- js/eventos.js
+++ js/eventos.js
@@ -8,13 +8,19 @@ BTN_LISTAR.addEventListener('click', function(){
   SECTION_LISTAR.style.display = 'block';
 
 
-BTN_LISTAR.classList.remove('btn-outline-dark');
-BTN_LISTAR.classList.add('btn-dark');
-BTN_CADASTRAR.classList.remove('btn-dark');
-BTN_CADASTRAR.classList.add('btn-outline-dark');
+  BTN_LISTAR.classList.remove('btn-outline-dark');
+  BTN_LISTAR.classList.add('btn-dark');
+  BTN_CADASTRAR.classList.remove('btn-dark');
+  BTN_CADASTRAR.classList.add('btn-outline-dark');
 });
 
 BTN_CADASTRAR.addEventListener('click', function(){
     SECTION_LISTAR.style.display = 'none';
     SECTION_CADASTRAR.style.display = 'block';
+
+    BTN_CADASTRAR.classList.remove('btn-outline-dark');
+    BTN_CADASTRAR.classList.add('btn-dark');
+    BTN_LISTAR.classList.remove('btn-dark');
+    BTN_LISTAR.classList.add('btn-outline-dark');
+
 });
