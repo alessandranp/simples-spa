970d06583d5a6879690ebb11cbdc6259cd59f440
La017 <luizm4artins@hotmail.com>
2021-09-18 09:49:43 -0300

    criar constantes

---
+++ eventos.js
@@ -0,0 +1,4 @@
+const BTN_CADASTRAR = document.grtElementById('btn-cadastrar');
+const BTN_LISTAR = document.grtElementById('btn-listar');
+const SECTION_CADASTRAR = document.grtElementById('btn-cadastrar');
+const SECTION_LISTAR = document.grtElementById('btn-listar');
