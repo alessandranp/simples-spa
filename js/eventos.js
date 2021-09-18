29ad623520c32d11977cabb2817ee5f5cf4dbbf2
La017 <luizm4artins@hotmail.com>
2021-09-18 10:27:32 -0300

    corbotao

--- js/eventos.js
+++ js/eventos.js
@@ -6,6 +6,12 @@ const SECTION_LISTAR = document.getElementById('section-listar');
 BTN_LISTAR.addEventListener('click', function(){
   SECTION_CADASTRAR.style.display = 'none';
   SECTION_LISTAR.style.display = 'block';
+
+
+BTN_LISTAR.classList.remove('btn-outline-dark');
+BTN_LISTAR.classList.add('btn-dark');
+BTN_CADASTRAR.classList.remove('btn-dark');
+BTN_CADASTRAR.classList.add('btn-outline-dark');
 });
 
 BTN_CADASTRAR.addEventListener('click', function(){
